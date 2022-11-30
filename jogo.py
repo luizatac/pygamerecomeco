6b627492e2f8659ad5e9837e68765a6918aee523
Luizatac <luizatac@al.insper.edu.br>
2022-11-30 00:20:39 -0300

    oi

--- jogo.py
+++ jogo.py
@@ -41,3 +41,55 @@ class Background:
 
     def draw(self, screen):
         screen.blit(self.image, self.pos)
+
+class Game:
+    screen = None
+    screen_size = None
+    run = True
+    menu_open = True
+    list = None
+    player = None
+    player_life = None
+    player_xp = None
+    background = None
+    img = None
+    latido = None
+    choro = None
+    fim = False
+    nivel = 20
+
+
+    def _init_(self, size):
+        actors = {}
+        pygame.init()
+        flags = DOUBLEBUF
+
+        self.screen = pygame.display.set_mode(size, flags)
+        self.screen_size = self.screen.get_size()
+        pygame.mouse.set_visible(0)
+        pygame.display.set_caption('BellyBaloo') 
+        self.load_images()
+        pygame.mixer.init()
+        self.latido = pygame.mixer.Sound(os.path.join('snd','latido.wav'))
+        self.latido.set_volume(0.5)
+
+        self.choro = pygame.mixer.Sound(os.path.join('snd','choro.wav'))
+        self.choro.set_volume(0.5) 
+
+
+    def load_images(self):
+        def load_image(filename):
+            img = pygame.image.load(os.path.join('imagens', filename))
+            img.set_alpha(None, RLEACCEL) 
+            img.convert()
+            img.set_colorkey(( 255, 0, 255), RLEACCEL)
+            return img
+        self.image_player_status = load_image('vidasc.png')
+
+    def handle_events(self):
+        player = self.player
+
+        for event in pygame.event.get():
+            t = event.type
+            if t in (KEYDOWN, KEYUP):
+                k=event.key
