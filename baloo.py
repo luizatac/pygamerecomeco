7140b193c09cc94b9bbd706ad9194880bfb78065
Luizatac <luizatac@al.insper.edu.br>
2022-11-30 00:10:04 -0300

    criei e e a classe das vidas tambem

--- baloo.py
+++ baloo.py
@@ -66,3 +66,72 @@ class Baloo(Player):
 
     def set_XP(self, XP):
         self.XP = XP
+
+class JogadorXPStatus:
+
+    font    = None
+    last_xp = -1
+    fgcolor = None
+    bgcolor = None
+    image   = None
+
+    def _init_(self, player, pos=None, font=None, ptsize=30, fgcolor="0xffff00", bgcolor=None):
+        self.player  = player
+        self.fgcolor = pygame.color.Color(fgcolor)
+        if bgcolor:
+            self.bgcolor = pygame.color.Color(bgcolor)
+        self.pos     = pos or [0, 0]
+        self.font    = pygame.font.Font(font, ptsize)
+
+        self.last_rect = None
+
+    def update(self, dt):
+        pass
+
+    def draw(self, screen):
+        text = "Score: % 4d" % self.player.get_XP()
+        if self.bgcolor:
+            self.image = self.font.render(text, False, self.fgcolor, self.bgcolor)
+        else:
+            self.image = self.font.render(text, False, self.fgcolor, (255, 0, 255))
+            self.image.set_colorkey((255, 0, 255), RLEACCEL)
+
+        self.last_rect = Rect(self.pos, self.image.get_size())
+
+        screen.blit(self.image, self.pos)
+
+    def clear(self, screen, background):
+        if self.last_rect:
+            screen.blit(background, self.last_rect)
+
+
+class JogadorLifeStatus:
+    player     = None
+    pos        = None
+    image      = None
+    size_image = None
+    spacing    = 5
+    def _init_(self, player, pos=None, image=None):
+        image = './imagens/vidasc.png'
+        self.image = pygame.image.load(image)
+
+        self.player     = player
+        self.pos        = pos or [ 5, 5 ]
+        self.size_image = self.image.get_size()
+        self.last_rect  = None
+
+    def update(self, dt):
+        pass
+
+    def draw(self, screen):
+        pos = copy.copy(self.pos)
+        for i in range(self.player.get_lives()):
+            pos[0] += self.size_image[0] + self.spacing
+            screen.blit(self.image, pos)
+
+        pos[1] = self.size_image[1]
+        self.last_rect = Rect(self.pos, pos)
+
+    def clear(self, screen, background):
+        if self.last_rect:
+            screen.blit(background,self.last_rect)
